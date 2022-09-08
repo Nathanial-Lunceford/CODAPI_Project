5ba106070732f8b17e808dfbd196e60a17f67d46
Nate <nathanial.lunceford@gmail.com>
2022-09-08 12:58:40 -0500

    Initial Commit

---
+++ CODApiTesting/GamerStats.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODApiTesting
+{
+    public class GamerStats
+    {
+        public double wins { get; set; }
+        public double kills { get; set; }
+        public double kdRatio { get; set; }
+        public double killstreak { get; set; }
+        public double level { get; set; }
+        public double accuracy { get; set; }
+        public double losses { get; set; }
+        public double prestige { get; set; }
+        public double hits { get; set; }
+        public double score { get; set; }
+        public double timePlayed { get; set; }
+        public double headshots { get; set; }
+        public double averageTime { get; set; }
+        public double gamesPlayed { get; set; }
+        public double assists { get; set; }
+        public double misses { get; set; }
+        public double xp { get; set; }
+        public double scorePerMinute { get; set; }
+        public double shots { get; set; }
+        public double deaths { get; set; }
+
+
+        public GamerStats(double winNum)
+        {
+            wins = winNum;
+        }
+
+        public void PrintVals(double winNum)
+        {
+            Console.WriteLine(wins);
+        }
+            
+    }
+        
+        
+}
---
+++ CODApiTesting/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using CODApiTesting;
+using System.Linq;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+
+		/*This is getting Cod results using HTTPClient*/
+
+
+
+		Console.WriteLine("What is your gamertag in Warzone?");
+		string gamertag = Console.ReadLine();
+
+		Console.WriteLine("What platform do you play on?" +
+            " psn for playstation, steam for Steam, " +
+            "battle for BattleNet, xbl for XBox,");
+		string platform = Console.ReadLine();
+
+		var uri = $"https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/{gamertag}/{platform}";
+
+		var client = new HttpClient();
+		var request = new HttpRequestMessage
+		{
+			Method = HttpMethod.Get,
+			RequestUri = new Uri(uri),
+			Headers =
+			{
+				{ "X-RapidAPI-Key", "613e1be4fcmshc7128b510340304p134544jsn7e3018cf4f57" },
+				{ "X-RapidAPI-Host", "call-of-duty-modern-warfare.p.rapidapi.com" },
+			},
+		};
+
+		using (var response = await client.SendAsync(request))
+		{
+			//response.EnsureSuccessStatusCode();
+			var body = await response.Content.ReadAsStringAsync();
+			
+			var entries = JObject.Parse(body)["br_all"].ToString();
+
+			//These next two lines get me the amount of wins the chosen player has and prints it
+			var win = JObject.Parse(entries).GetValue("wins");
+			Console.WriteLine($"You have {win} wins");
+
+			var kills = JObject.Parse(entries).GetValue("kills");
+			Console.WriteLine($"You have {kills} kills");
+
+			var kdRatio = JObject.Parse(entries).GetValue("kdRatio");
+			Console.WriteLine($"You have a {kdRatio} kd ratio");
+
+			var downs = JObject.Parse(entries).GetValue("downs");
+			Console.WriteLine($"You've downed {downs} enemies");
+
+			var topTwentyFive = JObject.Parse(entries).GetValue("topTwentyFive");
+			Console.WriteLine($"You've been in the top twenty five {topTwentyFive} times");
+
+			var topTen = JObject.Parse(entries).GetValue("topTen");
+			Console.WriteLine($"You've gotten into the top ten {topTen} times");
+
+			var contracts = JObject.Parse(entries).GetValue("contracts");
+			Console.WriteLine($"You've completed {contracts} contracts");
+
+			var revives = JObject.Parse(entries).GetValue("revives");
+			Console.WriteLine($"You've revived {revives} friendlies");
+
+			var topFive = JObject.Parse(entries).GetValue("topFive");
+			Console.WriteLine($"You've gotten into the top five {topFive} times");
+
+			var score = JObject.Parse(entries).GetValue("score");
+			Console.WriteLine($"Your total score is {score}");
+
+			var timePlayed = JObject.Parse(entries).GetValue("timePlayed");
+			Console.WriteLine($"You've played for {timePlayed} minutes");
+
+			var gamesPlayed = JObject.Parse(entries).GetValue("gamesPlayed");
+			Console.WriteLine($"You've played {gamesPlayed} games");
+
+			var scorePerMinute = JObject.Parse(entries).GetValue("scorePerMinute");
+			Console.WriteLine($"Your average score per minute is {scorePerMinute}");
+
+			var deaths = JObject.Parse(entries).GetValue("deaths");
+			Console.WriteLine($"You've died {deaths} times");
+		}
+	}
+}
